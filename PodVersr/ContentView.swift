58278ba7e0e6a11b7d0298ab7931d09c684567d3
Manikanta Sirumalla <Manikanta.sirumalla@icloud.com>
2023-12-11 12:39:18 +0530

    Initial Commit

---
+++ PodVersr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PodVersr
+//
+//  Created by Manikanta Sirumalla on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PodVersr/PodVersrApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodVersrApp.swift
+//  PodVersr
+//
+//  Created by Manikanta Sirumalla on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PodVersrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
